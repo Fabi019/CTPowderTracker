80494116035a50a1a6fe2925a9f7f95f90eab01c
Fabi019 <fabi019@gmx.de>
2022-07-11 12:10:47 +0200

    Refactor

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ statDisplay.setBackground("full");
 
 const moveGui = new Gui();
 
-const bossBar = Java.type("net.minecraft.entity.boss.BossStatus");
-function is2xEventActive() {
-  const bossBarText = bossBar.field_82827_c;
-  return bossBarText.includes("2X POWDER");
-}
-
-var sessionRunning = false;
-var startTime = 0;
-var sessionChests = 0;
-var sessionGemstone = 0;
-var sessionMithril = 0;
+let sessionRunning = false;
+let startTime = 0;
+let sessionChests = 0;
+let sessionGemstone = 0;
+let sessionMithril = 0;
 
 register("worldLoad", () => {
   updateDisplay();
@@ -70,15 +64,8 @@ register("command", (...args) => {
         return;
       }
       sessionRunning = false;
-      const millis = Date.now() - startTime;
-      let sec = Math.floor(millis / 1000);
-      let min = Math.floor(sec / 60);
-      let hr = Math.floor(min / 60);
-      sec %= 60;
-      min %= 60;
-      hr %= 24;
-      const pad = num => num.toString().padStart(2, "0");
-      ChatLib.chat(`Stopped powder session. Active time: ${pad(hr)}:${pad(min)}:${pad(sec)}`);
+      const [hr, min, sec] = getSessionDuration();
+      ChatLib.chat(`Stopped powder session. Active time: ${hr}:${min}:${sec}`);
       updateDisplay();
       break;
     case "sync":
@@ -133,10 +120,9 @@ function updateDisplay() {
   );
   statDisplay.setShouldRender(Settings.TrackerVisible);
 
-  let count = 0;
+  let line = 0;
   const renderText = (text, value) => {
-    statDisplay.setLine(count, `§6${text}: §r${value}`);
-    count++;
+    statDisplay.setLine(line++, `§6${text}: §r${value}`);
   };
 
   if (Settings.ShowTotals) {
@@ -144,7 +130,7 @@ function updateDisplay() {
     renderText("Total Gemstone", data.gemstonePowder);
     renderText("Total Mithril", data.mithrilPowder);
     if (sessionRunning) {
-      count++;
+      line++;
     }
   }
 
@@ -154,7 +140,7 @@ function updateDisplay() {
     renderText("Session Mithril", sessionMithril);
     
     if (Settings.ShowRates) {
-      count++;
+      line++;
       const elapsedSeconds = (Date.now() - startTime) / 1000;
       renderText("Chests/hr", Math.round((sessionChests/elapsedSeconds) * 3600));
       renderText("Gemstone/hr", Math.round((sessionGemstone/elapsedSeconds) * 3600));
@@ -171,3 +157,27 @@ register("dragged", (dx, dy, x, y) => {
   }
 });
 
+const overlayText = new Text("Drag to move the PowderTracker")
+  .setAlign("center")
+  .setScale(4)
+  .setShadow(true);
+moveGui.registerDraw(() => {
+  overlayText.draw(Renderer.screen.getWidth() / 2, Renderer.screen.getHeight() / 2);
+});
+
+function getSessionDuration() {
+  const millis = Date.now() - startTime;
+  let sec = Math.floor(millis / 1000);
+  let min = Math.floor(sec / 60);
+  let hr = Math.floor(min / 60) % 24;
+  sec %= 60;
+  min %= 60;
+  const pad = num => num.toString().padStart(2, "0");
+  return [pad(hr), pad(min), pad(sec)];
+}
+
+const bossBar = Java.type("net.minecraft.entity.boss.BossStatus");
+function is2xEventActive() {
+  const bossBarText = bossBar.field_82827_c; // bossName
+  return bossBarText.includes("2X POWDER");
+}
